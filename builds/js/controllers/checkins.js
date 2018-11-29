0db8acb5afe05fadbf28979dee4c0f0b3cb8486c
Rich Wertz <richardwertz@verizon.net>
2018-11-29 12:39:56 -0500

    Uploaded files manually
    Final app is finished. Adding notes and comments for next commit

--- builds/js/controllers/checkins.js
+++ builds/js/controllers/checkins.js
@@ -23,8 +23,42 @@ myApp.controller('CheckInsController', ['$scope', '$rootScope', '$firebaseObject
 
         $scope.pickRandom = function() {
             var whichRecord = Math.round(Math.random() * (checkinsList.length - 1));
-            $scope.recrodId = checkinsList.$keyAt(whichRecord);
+            $scope.recordId = checkinsList.$keyAt(whichRecord);
+            console.log($scope.recordId);
+
         } // pick a random user
+
+        $scope.resetPick = function() {
+            $scope.recordId = '';
+        } //make the recordId empty again to reveal the entire list
+
+        $scope.showLove = function(myCheckin) {
+            myCheckin.show = !myCheckin.show;
+
+            if(myCheckin.userState == 'expanded') {
+                myCheckin.userState = '';
+            } else {
+                myCheckin.userState = 'expanded';
+            }
+        }
+
+
+
+        $scope.giveLove = function(myCheckin, myGift) {
+            var refLove = ref.child(myCheckin.$id).child('awards');
+            var checkinsArray = $firebaseArray(refLove);
+
+            checkinsArray.$add({
+                name: myGift,
+                date: firebase.database.ServerValue.TIMESTAMP,
+            });
+        }
+
+        $scope.deleteLove = function(myCheckin, key) {
+            var refLove = ref.child(myCheckin.$id).child('awards').child(key);
+            var record = $firebaseObject(refLove);
+            record.$remove(key);
+        } //delete love
 
         $scope.addCheckIn = function() {
 
